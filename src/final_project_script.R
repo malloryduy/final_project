6902a2a6ed255d563734541709f35dae6a265dbb
mallory duy <mduy@vols.utk.edu>
2026-03-25 12:41:26 -0400

    Adding files and preliminary data

---
+++ src/final_project_script.R
@@ -0,0 +1,22 @@
+# Download Packages 
+library(tidyverse)
+library(readr)
+library(ggplot2)
+# Open CSV File 
+mi
+# Convert Date
+COVID19_Outcomes_Tennessee <- COVID19_Outcomes_Tennessee |>
+  mutate(
+    WEEK_END_DATE = as.Date(WEEK_END_DATE, format = "%Y %b %d %I:%M:%S %p"))
+# Make simple plot to see all cases overtime 
+#Aggregate Data 
+COVID_outcomes_summary <- COVID19_Outcomes_Tennessee |>
+  group_by(WEEK_END_DATE) |>
+  summarise(total_cases = sum(CAT_TOTALCASES, na.rm = TRUE))
+View(COVID_outcomes_summary)
+#Make plot
+ggplot(COVID_outcomes_summary, aes(x = WEEK_END_DATE, y= total_cases)) +
+  geom_line() + labs(
+    title = "COVID19 Cases Over Time in Tennessee", 
+    x= "Date",
+    y = "Total Cases")
